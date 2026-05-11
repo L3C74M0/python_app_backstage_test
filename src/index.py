69bfbca7ba242f7ec46f91233ecd18d92827d539
david.lectamo <juandavidlectamocaicedo@gmail.com>
2026-05-11 17:05:19 -0500

    First commit

---
+++ src/index.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify # type: ignore
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now()
+    })
+
+
+@app.route('/api/v1/health')
+def health():
+    return jsonify({
+        'status': "Working"
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
